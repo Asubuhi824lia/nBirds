27c80bedbfe29ea6df42cabcb77cae745f2010f3
Asubuhi824lia <der_anastasi84@mail.ru>
2026-06-07 22:40:47 +0700

    fix: field controls markup, add btn position

--- app/pages/admin/components/FieldBlock/FieldBlockControls.tsx
+++ app/pages/admin/components/FieldBlock/FieldBlockControls.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import type { FieldDataType } from "../types";
-import { IconButton, TextField } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 import { Add as AddIcon } from '@mui/icons-material';
 import { specInputStaticProps, textFieldBaseStyles, type SpecInputPropsType } from "../../utils/textFieldProps";
 
@@ -42,33 +42,48 @@ export const FieldBlockControls = ({
   return (
     // TODO: ограничить ширину контейнеру
     <div className="addition-list-controls">
-      <TextField
-        id={id}
-        label={label}
-        value={text}
-        onPaste={(e) => handleChangeInput(e.clipboardData.getData('text'))} // TODO: to check
-        margin="normal"
-        slotProps={{ inputLabel: { shrink: true } }}
-        // общее.
-        {...textFieldBaseStyles}
-        // зависимость от id
-        {...specInputProps[id]}
-        // зависимость от isMultilines
-        {...{
-          multiline: isMultilines,
-          minRows: isMultilines ? 3 : undefined,
-          onChange: (e) =>
-            isMultilines
-              ? setText(e.target.value)
-              : handleChangeInput(e.target.value),
-          size: isMultilines ? "medium" : "small",
-        }}
-      />
-      {isAdditionList && (
-        <IconButton id={`${id}Btn`} name="action" value="delete" onClick={addItemHandler}>
-          <AddIcon color="secondary" fontSize="small" />
-        </IconButton>
-      )}
+      <Grid container spacing={isAdditionList ? 0.5 : 0}>
+        <Grid size="grow">
+          <TextField
+            id={id}
+            label={label}
+            value={text}
+            onPaste={(e) => handleChangeInput(e.clipboardData.getData('text'))} // TODO: to check
+            margin="normal"
+            slotProps={{ inputLabel: { shrink: true } }}
+            // общее.
+            {...textFieldBaseStyles}
+            // зависимость от id
+            {...specInputProps[id]}
+            // зависимость от isMultilines
+            {...{
+              multiline: isMultilines,
+              minRows: isMultilines ? 3 : undefined,
+              onChange: (e) =>
+                isMultilines
+                  ? setText(e.target.value)
+                  : handleChangeInput(e.target.value),
+              size: isMultilines ? "medium" : "small",
+            }}
+          />
+        </Grid>
+        {isAdditionList && (
+          <Grid size="auto" direction="row" sx={{ display: "flex", alignItems: "center", pt: 2, pb: 1 }} >
+            <Button
+              id={`${id}Btn`}
+              name="action"
+              value="delete"
+              size="small"
+              variant="outlined"
+              color="secondary"
+              onClick={addItemHandler}
+              sx={{ height: "100%", minWidth: "fit-content" }}
+            >
+              <AddIcon color="secondary" fontSize="medium" />
+            </Button>
+          </Grid>
+        )}
+      </Grid>
     </div>
   );
 }
