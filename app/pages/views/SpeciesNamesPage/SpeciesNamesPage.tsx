487a41e7f8dc31bfc9d74d384716511bd24bdc76
Asubuhi824lia <der_anastasi84@mail.ru>
2026-07-17 10:24:54 +0700

    add: display families groups on cards

--- app/pages/views/SpeciesNamesPage/SpeciesNamesPage.tsx
+++ app/pages/views/SpeciesNamesPage/SpeciesNamesPage.tsx
@@ -9,7 +9,7 @@
  * «отряд: род —> виды»
  */
 
-import { List, ListItem } from "@mui/material"
+import { Card, CardContent, CardHeader, Grid, List, ListItem } from "@mui/material"
 // TODO: variable — fromLowerCase
 import { FamiliesGroups } from "./data/FamiliesGroups"
 
@@ -24,18 +24,38 @@ import { FamiliesGroups } from "./data/FamiliesGroups"
 
 export const SpeciesNamesPage = () => {
   return (
-    <section>
+    <section
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: "column",
+        width: '80%'
+      }}
+    >
       {/* TODO: header */}
 
-      <List>
-        {FamiliesGroups.map((group, index) =>
-          group.families.map((family, ind) => (
-            <ListItem key={`group-${index}-family-${ind}`}>
-              {family.name || family.latin_name} — {family.species_length}
-            </ListItem>
-          ))
-        )}
-      </List>
+      <Grid container spacing={1} sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}>
+        {FamiliesGroups.map((group, index) => (
+          <Card key={`group-${index}`} sx={{ width: 'fit-content', height: "fit-content" }}>
+            <CardHeader
+              component="center"
+              title={
+                `${group.min_species_length}`
+                + (group.max_species_length ? ` - ${group.max_species_length}` : '+')
+              }
+            />
+            <CardContent>
+              <List>
+                {group.families.map((family, ind) => (
+                  <ListItem key={`group-${index}-family-${ind}`}>
+                    {family.name || family.latin_name} — {family.species_length}
+                  </ListItem>
+                ))}
+              </List>
+            </CardContent>
+          </Card>
+        ))}
+      </Grid>
     </section>
   )
 }
