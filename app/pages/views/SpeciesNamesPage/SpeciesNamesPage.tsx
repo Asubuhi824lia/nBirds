4ab386195e6582ceff814d4be56dee0c7cda0587
Asubuhi824lia <der_anastasi84@mail.ru>
2026-07-18 13:02:56 +0700

    add: switch families groups by tabs

--- app/pages/views/SpeciesNamesPage/SpeciesNamesPage.tsx
+++ app/pages/views/SpeciesNamesPage/SpeciesNamesPage.tsx
@@ -9,9 +9,10 @@
  * «отряд: род —> виды»
  */
 
-import { Card, CardContent, CardHeader, Grid, List, ListItem } from "@mui/material"
+import { Box, Card, CardContent, CardHeader, Grid, List, ListItem, Tab, Tabs } from "@mui/material"
 // TODO: variable — fromLowerCase
 import { FamiliesGroups } from "./data/FamiliesGroups"
+import { useState } from "react"
 
 
 
@@ -23,6 +24,8 @@ import { FamiliesGroups } from "./data/FamiliesGroups"
 
 
 export const SpeciesNamesPage = () => {
+  const [tab, setTab] = useState<number>(0);
+
   return (
     <section
       style={{
@@ -32,30 +35,84 @@ export const SpeciesNamesPage = () => {
         width: '80%'
       }}
     >
-      {/* TODO: header */}
+      <Box
+        sx={{
+          borderBottom: 1,
+          display: 'flex',
+          justifyContent: "center",
+          borderColor: 'divider',
+          bgcolor: 'ThreeDLightShadow'
+        }}
+      >
+        <Tabs centered value={tab} onChange={(_, setValue) => setTab(setValue)} aria-label="basic tabs example">
+          {FamiliesGroups.map(({ min_species_length: min, max_species_length: max }, index) => (
+            <Tab
+              key={`tab-${index}`}
+              {...a11yProps(index)}
+              sx={{ bgcolor: tab !== index ? "ThreeDFace" : "Background" }}
+              label={
+                `${min}`
+                + (max ? ` - ${max}` : '+')
+              }
+            />
+          ))}
+        </Tabs>
+      </Box>
 
       <Grid container spacing={1} sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}>
         {FamiliesGroups.map((group, index) => (
-          <Card key={`group-${index}`} sx={{ width: 'fit-content', height: "fit-content" }}>
-            <CardHeader
-              component="center"
-              title={
-                `${group.min_species_length}`
-                + (group.max_species_length ? ` - ${group.max_species_length}` : '+')
-              }
-            />
-            <CardContent>
-              <List>
-                {group.families.map((family, ind) => (
-                  <ListItem key={`group-${index}-family-${ind}`}>
-                    {family.name || family.latin_name} — {family.species_length}
-                  </ListItem>
-                ))}
-              </List>
-            </CardContent>
-          </Card>
+          <CustomTabPanel key={`tab-group-${index}`} value={tab} index={index}>
+            <Card key={`group-${index}`} sx={{ width: 'fit-content', height: "fit-content" }}>
+              <CardHeader
+                component="center"
+                title={
+                  `${group.min_species_length}`
+                  + (group.max_species_length ? ` - ${group.max_species_length}` : '+')
+                }
+              />
+              <CardContent>
+                <List>
+                  {group.families.map((family, ind) => (
+                    <ListItem key={`group-${index}-family-${ind}`}>
+                      {family.name || family.latin_name} — {family.species_length}
+                    </ListItem>
+                  ))}
+                </List>
+              </CardContent>
+            </Card>
+          </CustomTabPanel>
         ))}
       </Grid>
     </section>
   )
+}
+
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
 }
