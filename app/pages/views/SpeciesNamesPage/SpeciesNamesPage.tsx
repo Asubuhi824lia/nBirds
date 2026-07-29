022deb5073258f8196b9cda7cbdc85176eba3f21
Asubuhi824lia <der_anastasi84@mail.ru>
2026-07-29 13:38:47 +0700

    write TZ for new Widget, Tabs color modifications, footer

--- app/pages/views/SpeciesNamesPage/SpeciesNamesPage.tsx
+++ app/pages/views/SpeciesNamesPage/SpeciesNamesPage.tsx
@@ -46,6 +46,12 @@ export const SpeciesNamesPage = () => {
   return (
     <section style={StylePageSection}>
       {/* TODO: может ли header быть частью, напрямую влияющей на содержание main? до какой степени?*/}
+      {/* TODO: уровни показа инфы о Selected Names
+        управляющий компонент: <input type="range">
+        1. Показ "цвета" присутствующих в "группе" "типов" имён
+        2. + показ в кружочках "цвета" кол-ва соответствующих имён + показ общего кол-ва пересекающихся "групп"
+        3. + показ в кружочках "цвета" кол-ва имён из данной "группы", пересекающихся с другими "группами"
+      */}
       <header>
         <Box sx={StyleHeaderTabsBox}>
           <Tabs centered value={tab} onChange={(_, setValue) => setTab(setValue)}>
@@ -56,9 +62,34 @@ export const SpeciesNamesPage = () => {
         </Box>
       </header>
 
+      <section>
+        {/* Widget CommonInfoGroup — Показ общей инфы по "группе"
+          
+          Бегунок фильтра "уровней" инфы — (по вычисляемости)
+          управляющий компонент: <input type="range">
+          1. статистическое
+            - для скольких видов указаны альтернативные имена
+            - для скольких видов указаны имена только на латыни
+            - для скольких видов русские имена указаны ТОЛЬКО для монорода/моновида (НЕ семейству)
+            - Наибольшее кол-во альтернативных названий
+            - Наименьшее кол-во альтернативных названий
+          2. общие совпадения с PartNames
+            - Перечень "без пересечений PartNames" с альтернативными именами 
+              —* с приведением групп/семейств, общим числом родов/видов
+            - Перечень "без пересечений PartNames" с главными именами 
+              —* с приведением групп/семейств, общим числом родов/видов
+            -* статистика по соотношениям главных имен к группам PartNames?
+            -* статистика по соотношениям альтернативных к группам PartNames?
+          3. пересечения PartNames
+            -* главные имена пересекающиеся с названиями других семейств (по тэгам - частям названий - PartNames)
+            -* альтернативные имена пересекающихся с названиями других семейств (по тэгам - частям названий - PartNames)
+            -* Наибольшее по кол-ву пересечений групп PartNames?
+        */}
+      </section>
+
       <main>
         <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} sx={{ justifyContent: "flex-end" }}>
-          <div style={StyleCenteredWrapper}>
+          <section style={StyleCenteredWrapper}>
             <Grid container spacing={1} sx={StyleMainGrid}>
               {FamiliesGroups.map((group, index) => (
                 <CustomTabPanel key={`tab-group-${index}`} index={index} value={tab}>
@@ -72,7 +103,7 @@ export const SpeciesNamesPage = () => {
                 </CustomTabPanel>
               ))}
             </Grid>
-          </div>
+          </section>
 
           <Container sx={StyleMainContainer}>
             <FiltersNaming filterGroups={filterGroups} />
@@ -81,6 +112,13 @@ export const SpeciesNamesPage = () => {
           </Container>
         </Stack>
       </main>
+
+      {/* На перспективу
+          TODO: Footer
+          1. указание используемой классификации + год создания классификации (+обновляемость?..)
+          2. Select для переключения между доступными классификациями систематики
+          3. домены/разделы сайтов-источников
+      */}
     </section >
   )
 }
