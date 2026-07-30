cc260c3d5d0751afa8fbf9f63a76d6d7f3dfe525
Asubuhi824lia <der_anastasi84@mail.ru>
2026-07-30 11:38:19 +0700

    fix: emprt data after getGroupsByNum func

--- app/pages/views/SpeciesNamesPage/utils.ts
+++ app/pages/views/SpeciesNamesPage/utils.ts
@@ -14,14 +14,24 @@ export function getGroupsByNum(
     // ADD FILTER BY NAME TYPES
     switch (typeNameLatin) {
       case "latin_names_without":
-        if (!family.name && !family.alternative_names?.length) return; // не добавлять в Map
+        if (!family.name && !family.alternative_names?.length)
+          return; // не добавлять в Map
         else break;
       case "latin_names_only":
         tempFamily.name = "";
         tempFamily.alternative_names = [];
         break;
     }
+
+    // divide to subgroups by N
+    const N = family.species_length; //результат поля в temp не меняется
+    const key = N < 10 ? N : (Math.floor(N / 10) * 10);
+    if (parts.has(key))
+      parts.set(key, [...parts.get(key) as FamilyGroupStruct[], tempFamily])
+    else
+      parts.set(key, [tempFamily])
   })
+
   // sort groups
   parts.forEach((part, key) => {
     const partSorted = part.sort((a, b) => a.species_length - b.species_length);
